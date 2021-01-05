9630caffd6003504742c4e73e4c9e24e138f5102
Lucas Silbernagel <lucas.silbernagel@gmail.com>
2021-01-04 21:43:29 -0500

    set up demo form for all results endpoint

--- client/src/components/Endpoints/Endpoints.js
+++ client/src/components/Endpoints/Endpoints.js
@@ -1,4 +1,5 @@
 import { StyledEndpoints } from './Endpoints.styled';
+import { StyledForm } from '../Form.styled';
 import { StyledButton } from '../Button.styled';
 import axios from 'axios';
 
@@ -7,35 +8,67 @@ function Endpoints() {
   function output(inp) {
     const allData = document.getElementById('allData');
     allData.innerHTML = inp;
-}
+  }
 
-function syntaxHighlight(json) {
-    json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
-    return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+-]?\d+)?)/g, function (match) {
-        var cls = 'number';
-        if (/^"/.test(match)) {
-            if (/:$/.test(match)) {
-                cls = 'key';
-            } else {
-                cls = 'string';
-            }
-        } else if (/true|false/.test(match)) {
-            cls = 'boolean';
-        } else if (/null/.test(match)) {
-            cls = 'null';
-        }
-        return '<span class="' + cls + '">' + match + '</span>';
-    });
-}
+  function syntaxHighlight(json) {
+      json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+      return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+-]?\d+)?)/g, function (match) {
+          var cls = 'number';
+          if (/^"/.test(match)) {
+              if (/:$/.test(match)) {
+                  cls = 'key';
+              } else {
+                  cls = 'string';
+              }
+          } else if (/true|false/.test(match)) {
+              cls = 'boolean';
+          } else if (/null/.test(match)) {
+              cls = 'null';
+          }
+          return '<span class="' + cls + '">' + match + '</span>';
+      });
+  }
 
-  const showAllData = () => {
-    axios.get('/api/v1/words')
+  const showData = () => {
+    
+    let _idVal = `${document.getElementById('_id').value}`;
+    let _id = "";
+    if (_idVal === "") {
+      _id = "";
+    } else {
+      _id = `_id=${document.getElementById('_id').value}&`;
+    };
+
+    let categoryVal = `${document.getElementById('category').value}`;
+    let category = "";
+    if (categoryVal === "") {
+      category = "";
+    } else {
+      category = `category=${document.getElementById('category').value}&`;
+    };
+
+    let numLettersVal = `${document.getElementById('numLetters').value}`;
+    let numLetters = "";
+    if (numLettersVal === "") {
+      numLetters = "";
+    } else {
+      numLetters = `numLetters=${document.getElementById('numLetters').value}&`;
+    };
+
+    let numSyllablesVal = `${document.getElementById('numSyllables').value}`;
+    let numSyllables = "";
+    if (numSyllablesVal === "") {
+      numSyllables = "";
+    } else {
+      numSyllables = `numSyllables=${document.getElementById('numSyllables').value}`;
+    };
+
+    axios.get(`/api/v1/words/?${_id}${category}${numLetters}${numSyllables}`)
       .then((res) => {
         let allData = Object.assign({}, res.data);
         let str = JSON.stringify(allData, undefined, 4);
         output(syntaxHighlight(str));
       });
-
 
 
     // setLoading(true)
@@ -57,13 +90,30 @@ function syntaxHighlight(json) {
         
   };
 
+  const handleKeyPress = (e) => {
+    if(e.key === 'Enter'){
+      showData();
+    }
+  }
+
   return (
     <main>
       <div className="wrapper">
         <StyledEndpoints>
-          <StyledButton onClick={showAllData}>
-            Data
-          </StyledButton>
+          <StyledForm>
+            <label htmlFor="_id" className="sr-only">_id (optional)</label>
+            <input id="_id" type="text" placeholder="_id (optional)" onKeyPress={handleKeyPress} />
+            
+            <label htmlFor="category" className="sr-only">Category (optional)</label>
+            <input id="category" type="text" placeholder="Category (optional)" onKeyPress={handleKeyPress} />
+
+            <label htmlFor="numLetters" className="sr-only">numLetters</label>
+            <input id="numLetters" type="number" min="1" placeholder="numLetters (optional)" onKeyPress={handleKeyPress} />
+
+            <label htmlFor="numSyllables" className="sr-only">numSyllables (optional)</label>
+            <input id="numSyllables" type="number" min="1" placeholder="numSyllables (optional)" onKeyPress={handleKeyPress} />
+          </StyledForm>
+          <StyledButton type="submit" onClick={showData}>Show Data ⬇</StyledButton>
           <pre id="allData"></pre>
         </StyledEndpoints>
       </div>
