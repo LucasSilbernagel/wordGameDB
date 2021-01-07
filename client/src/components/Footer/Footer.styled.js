9fe8da9e26e2e8fea94539071504d2459e9f36e6
Lucas Silbernagel <lucas.silbernagel@gmail.com>
2021-01-07 16:03:40 -0500

    updated paragraph styles

--- client/src/components/Footer/Footer.styled.js
+++ client/src/components/Footer/Footer.styled.js
@@ -3,6 +3,9 @@ import styled from 'styled-components';
 export const StyledFooter = styled.footer`
   padding: 20px 0px;
   p {
+    background: unset;
+    padding: unset;
+    border-radius: unset;
     text-transform: uppercase;
     text-align: center;
     font-size: 1.1rem;
