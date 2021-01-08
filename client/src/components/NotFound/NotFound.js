39198ad7eacf4c80db9c1571088a75ea48f7e627
Lucas Silbernagel <lucas.silbernagel@gmail.com>
2021-01-07 22:56:30 -0500

    added 404 page component

---
+++ client/src/components/NotFound/NotFound.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import StyledNotFound from './NotFound.styled';
+
+const NotFound = () => (
+  <StyledNotFound>
+    <h2>404 - Page not found!</h2>
+    <Link to="/">
+      ⬅ Go Home
+    </Link>
+  </StyledNotFound>
+);
+
+export default NotFound;
---
+++ client/src/components/NotFound/NotFound.styled.js
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+
+export const StyledNotFound = styled.div`
+  text-align: center;
+  height: 50vh;
+  h2 {
+    color: red;
+    font-size: 3rem;
+  }
+  a {
+    color: ${({ theme }) => theme.primaryLight};
+    background: ${({ theme }) => theme.secondaryDark};
+    padding: 10px;
+    text-transform: uppercase;
+    font-weight: 700;
+    font-size: 2rem;
+    text-decoration: none;
+    border-radius: 5px;
+    &:hover,
+    &:focus {
+      text-decoration: underline;
+    }
+  }
+`;
+
+export default StyledNotFound;
