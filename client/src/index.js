717fae36d732a02b8a97d505ce893c4b38a762eb
Lucas Silbernagel <lucas.silbernagel@gmail.com>
2024-05-30 22:55:16 -0400

    upgraded client packages

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,15 @@
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 import { BrowserRouter } from 'react-router-dom'
 
-ReactDOM.render(
+const domNode = document.getElementById('root');
+const root = createRoot(domNode);
+
+root.render(
   <BrowserRouter>
     <App />
   </BrowserRouter>,
-  document.getElementById('root')
 )
 
 // If you want to start measuring performance in your app, pass a function
--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname + "/client/build/index.html"));
 });
 
-app.use((err, req, res, next) => {
+app.use((err, _req, _res, next) => {
   console.log(err);
   next();
 });
